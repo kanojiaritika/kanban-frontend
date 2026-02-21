9bf8f2013370302d43e7ff326d1df9ba64881efa
Ritika <20004614@iitb.ac.in>
2026-02-21 15:27:34 +0530

    initial commit

---
+++ src/components/Register.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const Register = () => {
+
+    return (
+        
+        <div>
+
+        </div>
+    )
+
+}
+
+export default Register;
